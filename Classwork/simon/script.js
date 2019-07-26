85c37b25efcbb49402b54f6167baec2e82a6037a
Graham W <greysnaa@gmail.com>
2019-07-26 15:43:42 -0500

    works better

--- Classwork/simon/script.js
+++ Classwork/simon/script.js
@@ -1,14 +1,20 @@
-var centerButton = document.getElementById("center");
-var colorButtons = document.getElementsByClassName("square");
-
-var compPattern = [];
-centerButton.addEventListener("click", gameStart);
+let centerButton = document.getElementById("center");
+let colorButtons = document.getElementsByClassName("square");
+let compPattern = [];
 
 var gameCounter = 1;
+var userCounter = 0;
+
+for (var i = 0; i < colorButtons.length; i++){
+    colorButtons[i].addEventListener("click", userClick);
+};
+
+centerButton.addEventListener("click", gameStart);
+
 function gameStart(){
-    centerButton.innerText = gameCounter++;
+    canPlay = false;
+    centerButton.innerText = gameCounter;
     centerButton.removeEventListener("click", gameStart)
-    computerChoice();
     buttonPattern();
 }
 
@@ -18,8 +24,9 @@ function computerChoice(){
 }
 
 function buttonPattern(){
+    computerChoice();
     var counter = 0;
-    colorButtons[compPattern[counter]].classList.add("active");
+    window.setTimeout(function(){colorButtons[compPattern[counter]].classList.add("active")}, 100);
 
     var intervalRef = window.setInterval(inOrder, 1000);
     
@@ -30,17 +37,43 @@ function buttonPattern(){
             window.setTimeout(function(){colorButtons[compPattern[counter]].classList.add("active")}, 100);
         }
         else {
+            userCounter = 0;
+            gameCounter++;
             window.clearInterval(intervalRef);
             centerButton.addEventListener("click", gameStart)
         }
     }
 }
 
-for (var i = 0; i < colorButtons.length; i++){
-    colorButtons[i].addEventListener("click", userClick);
-}
-
 function userClick(e){
     e.target.classList.add("active");
-    window.setTimeout(function(){ e.target.classList.remove("active") }, 20)
+    window.setTimeout(function(){ e.target.classList.remove("active") }, 20);
+    var index = -1;
+    switch (e.target.id) {
+        case "green":
+            index = 0;
+            break;
+        case "red":
+            index = 1;
+            break;
+        case "blue":
+            index = 2;
+            break;
+        case "yellow":
+            index = 3;
+    }
+    if (index == compPattern[userCounter]){
+        console.log(true);
+        userCounter++;
+        if ( userCounter == gameCounter - 1){
+            gameStart();
+        }
+    } else {
+        console.log(false);
+        centerButton.innerText = `You Lose! Try again?`;
+        compPattern = [];
+        gameCounter = 1;
+        userCounter = 0;
+
+    }
 }
